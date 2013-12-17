f51a0997e9c1702465aa8156b549141e2f96813f
Ben Peachey <potherca@gmail.com>
2013-12-18 00:05:03 +0100

    Improvement to display visual diff.

---
+++ class.XmlSorter.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Controller for the web frontend.
+ * 
+ * 
+ */
+class XmlSorter
+{
+    static public function forFile($p_sXmlPath)
+    {
+        if (is_file($p_sXmlPath) === false) {
+            throw new InvalidArgumentException('Giveb path "' . $p_sXmlPath . '"is not a file');
+        } else {
+            $sXml = file_get_contents($p_sXmlPath);
+            $oSorter = new self($sXml);
+            return $oSorter->sortXml($sXml);
+        }
+    }
+    
+    public function __contruct($p_sXml)
+    {
+        $this->m_sOriginalXml = $p_sXml;
+    }
+    
+    public function formatXml($p_sXml){
+        $oDocument = new DOMDocument('1.0');
+        $oDocument->preserveWhiteSpace = false;
+        $oDocument->formatOutput = true;
+        $oDocument->loadXML($p_sXml);
+        return $oDocument->saveXML();
+    }
+    
+    public function sortXml($p_sXml){
+        $aSortedXml = $this->xmlStringToSortedArray($p_sXml);
+        $sXml = $this->arrayToXml($aSortedXml);
+        return $this->formatXml($sXml);
+    }
+    
+    
+    protected function arrayToXml(array $p_aSubject) {
+        $p_oXml = new SimpleXMLElement('<?xml version="1.0"?><root></root>');        
+        $oXml = $this->arrayToXmlRecursive($p_aSubject, $p_oXml);
+        //@TODO: Get rid of ROOT node
+        return $oXml->asXml();
+    }
+
+    /**
+     * @url http://pastebin.com/pYuXQWee
+     * @see http://stackoverflow.com/a/5965940/153049
+     */
+    protected function arrayToXmlRecursive($p_aSubject, $p_oXml) {
+        foreach($p_aSubject as $t_sKey => $t_mValue) {
+            //$t_sKey = (is_numeric($t_sKey) ? 'item':'') . $t_sKey; // Uncomment if you need to fix numeric keys in the array
+            if(is_array($t_mValue)) {
+                $oSubNode = $p_oXml->addChild($t_sKey);
+                $this->arrayToXmlRecursive($t_mValue, $oSubNode);
+            }
+            else {
+                $p_oXml->addChild($t_sKey, $t_mValue);
+            }
+        }
+        
+        return $p_oXml;
+    }
+
+    protected function xmlToArray($p_sXml){
+        $xml = simplexml_load_string($p_sXml);
+        $json = json_encode($xml);
+        $array = json_decode($json,TRUE);
+        
+        return $array;
+    }
+    
+    protected function arraySortByKeysRecursive($p_aSubject){
+        ksort($p_aSubject);
+        foreach($p_aSubject as $t_sKey => $t_mValue){
+            if(is_array($t_mValue)){
+                $p_aSubject[$t_sKey] = $this->arraySortByKeysRecursive($t_mValue);
+            }
+        }
+        
+        return $p_aSubject;
+    }
+    
+    protected function xmlStringToSortedArray($p_sXmlString){
+        $aXml = $this->xmlToArray($p_sXmlString);
+        $aXml = $this->arraySortByKeysRecursive($aXml);
+        return $aXml;
+    }
+}   
+
+#EOF
--- index.php
+++ index.php
@@ -1,17 +1,115 @@
+<!DOCTYPE html>
+<?php
+    if (isset($_POST['display-mode'])) {
+        $sContent = getContent($_FILES, $_POST['display-mode']);
+    } else {
+        $sContent = getContent($_FILES);
+    }
+?>
+<html>
+<head>
+    <title>Compare two XML files</title>
+	<link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <!-- -->
+    <form action="" method="post" enctype="multipart/form-data">
+        <p>XML Files to compare:</p>
+        <p>
+            <label>
+                Old: 
+                <input name="left" type="file" />
+            </label>
+            <label>
+                New:
+                <input name="right" type="file" />
+            </label>
+        </p>
+        <p>
+            Display Diff
+            <label>
+                <input type="radio" name="display-mode" value="side-by-side" /> Side-by-side
+            </label>
+            <label>
+                <input type="radio" name="display-mode" value="inline" /> Inline
+            </label>
+        </p>
+        <p>
+            <input type="submit" value="Send files" />
+        </p>
+    </form>
+    <!-- -->
+    <?= $sContent?>
+</body>
+</html>
 <?php
 
-require 'class.DiffXml.php';
+function getContent(array $p_aUploadedFiles, $p_sDisplayType = 'side-by-side'){
+    $sContent = '';
+    
+    if (empty($p_aUploadedFiles)) {
+        // Nothing to do
+    } else {
+        if ($p_aUploadedFiles['left']['error'] === 4 OR $p_aUploadedFiles['right']['error'] === 4) {
+            $sContent = '<p class="warning">Please select 2 XML files to compare</p>';
+        } else {
+            if (!is_uploaded_file($p_aUploadedFiles['left']['tmp_name']) OR ! is_uploaded_file($p_aUploadedFiles['right']['tmp_name'])) {
+                $sContent = '<p class="warning">Uploaded files not legally uploaded</p>';
+            } else {
+                if ($_FILES['left']['error'] !== UPLOAD_ERR_OK OR $_FILES['right']['error'] !== UPLOAD_ERR_OK) { 
+                    $aErrors = array( 
+                            //  0 => 'There is no error, the file uploaded with success' 
+                              1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini' 
+                            , 2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form' 
+                            , 3 => 'The uploaded file was only partially uploaded'
+                            // , 4 => 'No file was uploaded'
+                            , 6 => 'Missing a temporary folder'
+                    );
+                    $sContent = '<p class="warning">'
+                        . 'There was an error uploading the files'
+                        . 'Left : ' . $aErrors[$_FILES['left']['error']]
+                        . 'Right: ' . $aErrors[$_FILES['right']['error']]
+                        . '</p>'
+                    ;
+                } else {
+		            require __DIR__ . '/class.XmlSorter.php';
+		            require __DIR__ . '/vendor/phpspec/php-diff/lib/Diff.php';
+		            require __DIR__ . '/vendor/phpspec/php-diff/lib/Diff/Renderer/Html/SideBySide.php';
+		            require __DIR__ . '/vendor/phpspec/php-diff/lib/Diff/Renderer/Html/Inline.php';
 
-$oXmlDiff = new XmlDiff();
+                    $sLeftXml = XmlSorter::forFile($p_aUploadedFiles['left']['tmp_name']);
+                    $sRightXml = XmlSorter::forFile($p_aUploadedFiles['right']['tmp_name']);
 
-$aDiffOne = $oXmlDiff->compare('1.xml', '2.xml');    
-$aDiffTwo = $oXmlDiff->compare('2.xml', '3.xml');    
+            		$aLeft = explode("\n", $sLeftXml);
+            		$aRight = explode("\n", $sRightXml);
 
-echo '<pre>'
-    . 'Diff between 1 and 2: '
-    . htmlentities(var_export($aDiffOne, true)) . PHP_EOL
-    . 'Diff between 2 and 3: '
-    . htmlentities(var_export($aDiffTwo, true))
-;
+            		$aOptions = array(
+		                //'context' => 999,
+		                'ignoreNewLines' => true,
+		                'ignoreWhitespace' => true,
+		                'ignoreCase' => false
+                    );
+
+		            // Initialize the diff class
+		            $oDiff = new Diff($aLeft, $aRight, $aOptions);
+
+		            // Generate a side by side diff
+                    if ($p_sDisplayType === 'side-by-side') {
+    		            $oRenderer = new Diff_Renderer_Html_SideBySide;
+                    } elseif($p_sDisplayType === 'inline') {
+                        $oRenderer = new Diff_Renderer_Html_Inline;
+                    } else {
+                        throw new InvalidARgumentException('Unknow display type "' . $p_sDisplayType . '"');
+                    }
+
+		            $sContent = $oDiff->render($oRenderer);
+
+                }
+            }
+        }
+    }
+    
+    return $sContent;
+}
 
 #EOF
