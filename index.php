8cca3ec415a3b478aea0f5a282f8ff2429ca1ac0
Ben Peachey <potherca@gmail.com>
2013-12-19 21:38:53 +0100

    Moves web resources to web directory.

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php
-ob_start();
-
-$sRootPath = __DIR__;
-
-require $sRootPath . '/lib/class.XmlSorter.php';
-require $sRootPath . '/lib/class.Controller.php';
-require $sRootPath . '/lib/class.Template.php';
-require $sRootPath . '/vendor/phpspec/php-diff/lib/Diff.php';
-require $sRootPath . '/vendor/phpspec/php-diff/lib/Diff/Renderer/Html/SideBySide.php';
-require $sRootPath . '/vendor/phpspec/php-diff/lib/Diff/Renderer/Html/Inline.php';
-
-$sDisplayMode = isset($_POST['display-mode']) && empty($_POST['display-mode']) === false
-    ? $_POST['display-mode']
-    :'side-by-side'
-;
-
-
-try {
-    $oController = new Controller();
-    $sContent = $oController->getContent($_FILES, $sDisplayMode);
-
-    $oTemplate = Template::fromFile('template.index.html');
-
-    $sContent = str_replace('<div id="content"></div>', $sContent, $oTemplate->toString());
-} catch(Exception $eAny){
-    var_dump($eAny);
-    $sContent = '<p class="warning">' . $eAny->getMessage() . '</p>';
-}
-$sBufferedContents = ob_get_contents();
-
-echo $sContent;
+echo '<pre>';
+echo `pwd`;
+echo `ls -Rl ../`;
 
 #EOF
